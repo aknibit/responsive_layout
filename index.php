427a6a3d97df8732384eb561505697a3925ff243
Jonathan <jonathan@aknibit.com>
2012-11-14 18:45:34 +0100

    upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Friktrip APP - Descárgala ya en tu iPhone - Instant Travel Sharing</title>
+<meta name="keywords" content="instant travel sharing" />
+<meta name="description" content="¿Estás cansado de ordenar y revisar las fotos después de tus viajes? ¿Quieres guardar y compartir tus recuerdos mientras viajas? INSTANT TRAVEL SHARING" />
+<meta name="viewport" content="width=device-width; initial-scale=1.0" /> 
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" /> 
+<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script> 
+</head>
+<body>
+
+<!-- content -->
+<div id="section">
+	<div class="bg" style="margin:50px 0;">
+		<div class="container">
+			<div class="wrapper">
+			
+				<div class="grid_top suffix top2">
+					<div class="box-img"><img src="img/instanttravelsharing.png" alt="isntant travel sharing" /></div>
+				</div>
+
+				<div class="grid_top suffix top2">
+				<div style="margin-left:20px;">
+					<h1 class="top">Instant Travel Sharing</h1>
+					<h2 class="bot">Tu diario de viaje en tiempo real</h2>
+					<p class="bot">¿Estás cansado de ordenar y revisar las fotos después de tus viajes? ¿Quieres guardar y compartir tus recuerdos mientras viajas?</p>
+					<p class="bot"><strong>INSTANT</strong> - Guarda y publica tus fotos en segundos<br />
+					<strong>TRAVEL</strong> - Crea tu diario mientras viajas sin necesidad de conexión de datos y geolocalizado<br />
+					<strong>SHARING</strong> - Compártelo en Friktrip y en tus redes sociales favoritas automáticamente<br />
+					</p>
+					<a href="http://m.friktrip.com"><img style="margin-bottom:25px;" src="img/app-store.png" alt="app store" /></a>
+				</div>
+				</div>
+				
+				<div class="clear"></div> 
+				
+			</div>
+		</div>
+	</div>
+
+	<div class="bg">
+	<div class="container padRT">
+		<div class="wrapper">
+			<div class="grid_bot suffix bot2">
+				<div class="box">
+					<img src="img/3page_img1.png" alt="" />
+					<a href="#" class="link1">DIARIO DE VIAJE</a>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+				<div class="box">
+					<img src="img/3page_img2.png" alt="" />
+					<a href="#" class="link1">RÁPIDO Y FÁCIL</a>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+			</div>
+			<div class="grid_bot suffix bot2">
+				<div class="box">
+					<img src="img/3page_img3.png" alt="" />
+					<a href="#" class="link1">VERSIÓN WEB</a>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+				<div class="box">
+					<img src="img/3page_img4.png" alt="" />
+					<a href="#" class="link1">GEOLOCALIZADO</a>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+			</div>
+			<div class="grid_bot suffix bot2">
+				<div class="box">
+					<img src="img/3page_img5.png" alt="" />
+					<a href="#" class="link1">SIN CONEXIÓN NI ROAMING</a>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+				<div class="box">
+					<img src="img/3page_img6.png" alt="" />
+					<a href="#" class="link1">100% SOCIAL</a>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+<!-- footer -->
+<div id="footer">
+	<div class="container"> 
+		<div class="wrapper">
+			<div><a href="http://friktrip.com" class="link"><img style="margin-top:-1px;" src="img/logo-footer.png" alt="friktrip" /></a> &copy; 2012 | <a href="http://friktrip.com">Visita friktrip.com</a></div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
